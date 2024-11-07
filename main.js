fdd8b3f665f71dd5aa4bb5c855c80e50ce7934bc
Cemus <24947582+Cemus@users.noreply.github.com>
2024-11-07 10:01:30 +0100

    First draft

---
+++ main.js
@@ -0,0 +1,98 @@
+import "./style.css";
+
+/*
+Exo 1 : déclarer des variables de différents types (string, number, boolean) et d'afficher leur contenu dans la console.
+Exo 2 : Écrire un script qui convertit des degrés Celsius en Fahrenheit.
+Conditions et boucles :
+Exo 3 : Écrire une condition pour vérifier si un nombre est pair ou impair, et afficher un message correspondant.
+Exo 4 : Utiliser une boucle for pour afficher les nombres de 1 à 10, puis inverser la boucle pour les afficher de 10 à 1.
+Fonctions :
+Exo 5 : créer une fonction simple qui prend deux nombres et renvoie leur somme.
+Exo 6 : Créer une fonction pour vérifier si une chaîne de caractères contient un certain mot.
+Tableaux et objets :
+Exo 7 : Créer un tableau de prénoms et afficher chacun avec une boucle.
+Exo 8: créer un objet simple représentant une personne (nom, âge, ville) et d'accéder aux propriétés. (On affiche en console)
+Manipulation de DOM :
+Exo 9 :ajouter un élément de texte dans une page HTML via JavaScript.
+Exo 10 : Créer un bouton via JS, on l’affiche dans la page et au click cela déclenche une alerte
+*/
+
+//Exo 1
+let exerciceNumber = 1;
+let characters = "Salut Jeff";
+let isBoolean = true;
+
+console.log(
+  `exo : ${exerciceNumber}, chaîne : ${characters}, booléen : ${isBoolean}`
+);
+
+//Exo 2
+function celsiusToFarenheit(temp) {
+  return temp * (9 / 5) + 32;
+}
+
+//Exo 3
+function isEven(number) {
+  if (number % 2 == 0) {
+    console.log("Pair !");
+    return true;
+  } else {
+    console.log("Impair !");
+    return false;
+  }
+}
+
+//Exo 4
+function loopAndReversedLoop() {
+  let index = 1;
+  for (; index < 10; index++) {
+    console.log(index);
+  }
+  for (; index > 0; index--) {
+    console.log(index);
+  }
+}
+
+console.log(loopAndReversedLoop());
+
+//Exo 5
+function sum(a, b) {
+  return a + b;
+}
+
+//Exo 6
+function hasWord(string, word) {
+  return string.includes(word);
+}
+
+//Exo 7
+const names = ["Siegfried", "Jill", "Mario", "Domitille", "Dante"];
+for (let index = 0; index < names.length; index++) {
+  const element = names[index];
+  console.log(element);
+}
+
+//Exo 8
+const person = {
+  name: "Raymond",
+  age: 48,
+  city: "Toulouse",
+};
+console.log(
+  `Il s'appelle ${person.name}, il a ${person.age}, il habite a ${person.city}`
+);
+
+//Exo 9
+const body = document.body;
+console.log(body);
+const p = document.createElement("p");
+p.textContent = "Paragraphe totalement artificiel";
+body.appendChild(p);
+
+//Exo 10
+const button = document.createElement("button");
+button.textContent = "Bouton manufacturé";
+button.addEventListener("click", () => {
+  alert("coucou");
+});
+body.appendChild(button);
